fea3c96d1aa11d54f6fd8f6207dc7b88c0a22f22
Lidor <lidorsoikher012@gmail.com>
2018-07-06 02:23:43 +0000

    securittyyy

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -6,3 +6,4 @@ exit(0);
 ?>
 1530842133_2514:"Warning: goodness, 72.219.137.136; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.18; sucuri-scanner\/sucuri.php)"
 1530842781_4801:"Warning: 93.115.95.205; User account created; ID: 16387; name: edotson21995; email: edotson2@cox.net; roles: subscriber"
+1530843731_3494:"Info: goodness, 72.219.137.136; Core file restored:\/home\/lidorsoikher012\/ogi\/\/wp-blog-header.php"
