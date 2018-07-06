03de469b6eeabec9038514d203ea24b28d28cb12
Lidor <lidorsoikher012@gmail.com>
2018-07-06 02:56:44 +0000

    looks clean2

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -10,3 +10,4 @@ exit(0);
 1530843873_0411:"Info: goodness, 72.219.137.136; Core file restored:\/home\/lidorsoikher012\/ogi\/\/wp-admin\/includes\/class-wp-upgrader.php"
 1530844131_2523:"Critical: goodness, 72.219.137.136; WordPress updated to version: 4.9.7"
 1530844257_7403:"Warning: goodness, 72.219.137.136; Themes updated: (multiple entries): Twenty Eleven (v2.5; twentyeleven),Twenty Fifteen (v1.7; twentyfifteen),Twenty Fourteen (v1.9; twentyfourteen),Twenty Ten (v2.2; twentyten),Twenty Thirteen (v2.1; twentythirteen),Twenty Twelve (v2.2; twentytwelve),Flatsome (v3.3.8; flatsome)"
+1530845335_6249:"Warning: 178.175.135.101; User account created; ID: 16388; name: webmaster2002; email: webmaster@centuryconcreteinc.com; roles: subscriber"
