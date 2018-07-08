34fa0aeb185eedcb9cf3eb9491454bf396ba36d1
Lidor <lidorsoikher012@gmail.com>
2018-07-08 00:57:13 +0000

    broken with logs

---
+++ wp-content/uploads/sucuri/sucuri-hookdata.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=hookdata;
+// created_on=1530899914;
+// updated_on=1530899914;
+exit(0);
+?>
--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -1,1 +1,16 @@
 <?php exit(0); ?>
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-06 08:37:47"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-06 08:37:47"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.201.226","user_hostname":"69.50.201.226","user_lastlogin":"2018-07-06 08:53:29"}
+{"user_id":2032,"user_login":"shinobi069","user_remoteaddr":"98.110.128.68","user_hostname":"pool-98-110-128-68.bstnma.east.verizon.net","user_lastlogin":"2018-07-06 09:24:19"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-06 09:34:28"}
+{"user_id":2042,"user_login":"courtney","user_remoteaddr":"67.253.2.69","user_hostname":"cpe-67-253-2-69.maine.res.rr.com","user_lastlogin":"2018-07-06 13:16:59"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-06 15:57:24"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.203.24","user_hostname":"69.50.203.24","user_lastlogin":"2018-07-06 18:14:35"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.203.21","user_hostname":"69.50.203.21","user_lastlogin":"2018-07-07 03:03:24"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.205.177","user_hostname":"69.50.205.177","user_lastlogin":"2018-07-07 07:06:26"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.201.222","user_hostname":"69.50.201.222","user_lastlogin":"2018-07-07 07:39:21"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-07 11:33:31"}
+{"user_id":1905,"user_login":"mtvonthadden","user_remoteaddr":"31.207.2.130","user_hostname":"31.207.2.130","user_lastlogin":"2018-07-07 12:27:38"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"69.50.203.23","user_hostname":"69.50.203.23","user_lastlogin":"2018-07-07 16:05:02"}
+{"user_id":1,"user_login":"goodness","user_remoteaddr":"72.219.144.212","user_hostname":"ip72-219-144-212.oc.oc.cox.net","user_lastlogin":"2018-07-07 17:56:00"}
